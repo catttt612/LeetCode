59a076b827180d38460a62b8fe4a350924506ef2
catttt612 <catttt612@gmail.com>
2020-06-15 23:17:47 +0800

    94. Binary Tree Inorder Traversal

---
+++ src/main/java/com/catttt/leetcode/editor/en/BinaryTreeInorderTraversal.java
@@ -0,0 +1,69 @@
+//Given a binary tree, return the inorder traversal of its nodes' values. 
+//
+// Example: 
+//
+// 
+//Input: [1,null,2,3]
+//   1
+//    \
+//     2
+//    /
+//   3
+//
+//Output: [1,3,2] 
+//
+// Follow up: Recursive solution is trivial, could you do it iteratively? 
+// Related Topics Hash Table Stack Tree
+
+package com.catttt.leetcode.editor.en;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class BinaryTreeInorderTraversal {
+    public static void main(String[] args) {
+        Solution solution = new BinaryTreeInorderTraversal().new Solution();
+        TreeNode root = new TreeNode(1);
+        TreeNode node2 = new TreeNode(2);
+        TreeNode node3 = new TreeNode(3);
+        root.right = node2;
+        node2.left = node3;
+        List<Integer> result = solution.inorderTraversal(root);
+        System.out.println(result);
+    }
+//leetcode submit region begin(Prohibit modification and deletion)
+
+    /**
+     * Definition for a binary tree node.
+     * public class TreeNode {
+     * int val;
+     * TreeNode left;
+     * TreeNode right;
+     * TreeNode() {}
+     * TreeNode(int val) { this.val = val; }
+     * TreeNode(int val, TreeNode left, TreeNode right) {
+     * this.val = val;
+     * this.left = left;
+     * this.right = right;
+     * }
+     * }
+     */
+    class Solution {
+        public List<Integer> inorderTraversal(TreeNode root) {
+            List<Integer> result = new LinkedList<>();
+            traversal(result, root);
+            return result;
+        }
+
+        private void traversal(List<Integer> result, TreeNode node) {
+            if (node == null) {
+                return;
+            }
+            traversal(result, node.left);
+            result.add(node.val);
+            traversal(result, node.right);
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
