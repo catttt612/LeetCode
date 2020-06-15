fd5378a798de767564571d847dcbce18fe677bc2
catttt612 <catttt612@gmail.com>
2020-06-15 22:57:27 +0800

    242. Valid Anagram

---
+++ src/main/java/com/catttt/leetcode/editor/en/ValidAnagram.java
@@ -0,0 +1,48 @@
+//Given two strings s and t , write a function to determine if t is an anagram o
+//f s. 
+//
+// Example 1: 
+//
+// 
+//Input: s = "anagram", t = "nagaram"
+//Output: true
+// 
+//
+// Example 2: 
+//
+// 
+//Input: s = "rat", t = "car"
+//Output: false
+// 
+//
+// Note: 
+//You may assume the string contains only lowercase alphabets. 
+//
+// Follow up: 
+//What if the inputs contain unicode characters? How would you adapt your soluti
+//on to such case? 
+// Related Topics Hash Table Sort
+
+package com.catttt.leetcode.editor.en;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ValidAnagram {
+    public static void main(String[] args) {
+        Solution solution = new ValidAnagram().new Solution();
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public boolean isAnagram(String s, String t) {
+            int[] alphabet = new int[26];
+            for (int i = 0; i < s.length(); i++) alphabet[s.charAt(i) - 'a']++;
+            for (int i = 0; i < t.length(); i++) alphabet[t.charAt(i) - 'a']--;
+            for (int i : alphabet) if (i != 0) return false;
+            return true;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
