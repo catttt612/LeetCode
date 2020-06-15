4831894dbc08ee5ba364bea1c705f074e35674ba
catttt612 <catttt612@gmail.com>
2020-06-15 23:50:54 +0800

    589. N-ary Tree Preorder Traversal

---
+++ src/main/java/com/catttt/leetcode/editor/en/NAryTreePreorderTraversal.java
@@ -0,0 +1,94 @@
+//Given an n-ary tree, return the preorder traversal of its nodes' values. 
+//
+// Nary-Tree input serialization is represented in their level order traversal, 
+//each group of children is separated by the null value (See examples). 
+//
+// 
+//
+// Follow up: 
+//
+// Recursive solution is trivial, could you do it iteratively? 
+//
+// 
+// Example 1: 
+//
+// 
+//
+// 
+//Input: root = [1,null,3,2,4,null,5,6]
+//Output: [1,3,5,6,2,4]
+// 
+//
+// Example 2: 
+//
+// 
+//
+// 
+//Input: root = [1,null,2,3,4,5,null,null,6,7,null,8,null,9,10,null,null,11,null
+//,12,null,13,null,null,14]
+//Output: [1,2,3,6,7,11,14,4,8,12,5,9,13,10]
+// 
+//
+// 
+// Constraints: 
+//
+// 
+// The height of the n-ary tree is less than or equal to 1000 
+// The total number of nodes is between [0, 10^4] 
+// 
+// Related Topics Tree
+
+package com.catttt.leetcode.editor.en;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class NAryTreePreorderTraversal {
+    public static void main(String[] args) {
+        Solution solution = new NAryTreePreorderTraversal().new Solution();
+    }
+//leetcode submit region begin(Prohibit modification and deletion)
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+    class Solution {
+        public List<Integer> preorder(Node root) {
+            List<Integer> result = new LinkedList<>();
+            if (root == null) {
+                return result;
+            }
+            result.add(root.val);
+            traversal(result, root.children);
+            return result;
+        }
+
+        private void traversal(List<Integer> result, List<Node> children) {
+            if (children.isEmpty()) {
+                return;
+            }
+            for (int i = 0; i < children.size(); ) {
+                Node node = children.get(i++);
+                result.add(node.val);
+                traversal(result, node.children);
+            }
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
