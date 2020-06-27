1430e6dd6d68d9554b3c9ec1ca315783788cd1d1
catttt612 <catttt612@gmail.com>
2020-06-27 23:59:03 +0800

    116. Populating Next Right Pointers in Each Node

---
+++ src/main/java/com/catttt/leetcode/editor/en/PopulatingNextRightPointersInEachNode.java
@@ -0,0 +1,118 @@
+//You are given a perfect binary tree where all leaves are on the same level, an
+//d every parent has two children. The binary tree has the following definition: 
+//
+// 
+//struct Node {
+//  int val;
+//  Node *left;
+//  Node *right;
+//  Node *next;
+//}
+// 
+//
+// Populate each next pointer to point to its next right node. If there is no ne
+//xt right node, the next pointer should be set to NULL. 
+//
+// Initially, all next pointers are set to NULL. 
+//
+// 
+//
+// Follow up: 
+//
+// 
+// You may only use constant extra space. 
+// Recursive approach is fine, you may assume implicit stack space does not coun
+//t as extra space for this problem. 
+// 
+//
+// 
+// Example 1: 
+//
+// 
+//
+// 
+//Input: root = [1,2,3,4,5,6,7]
+//Output: [1,#,2,3,#,4,5,6,7,#]
+//Explanation: Given the above perfect binary tree (Figure A), your function sho
+//uld populate each next pointer to point to its next right node, just like in Fig
+//ure B. The serialized output is in level order as connected by the next pointers
+//, with '#' signifying the end of each level.
+// 
+//
+// 
+// Constraints: 
+//
+// 
+// The number of nodes in the given tree is less than 4096. 
+// -1000 <= node.val <= 1000 
+// 
+// Related Topics Tree Depth-first Search
+
+package com.catttt.leetcode.editor.en;
+
+public class PopulatingNextRightPointersInEachNode {
+    public static void main(String[] args) {
+        Solution solution = new PopulatingNextRightPointersInEachNode().new Solution();
+    }
+//leetcode submit region begin(Prohibit modification and deletion)
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public Node left;
+    public Node right;
+    public Node next;
+
+    public Node() {}
+    
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, Node _left, Node _right, Node _next) {
+        val = _val;
+        left = _left;
+        right = _right;
+        next = _next;
+    }
+};
+*/
+
+    class Solution {
+        public Node connect(Node root) {
+            if (root == null) return null;
+            helper(root);
+            return root;
+        }
+
+        private void helper(Node node) {
+            if (node == null) return;
+            if (node.left != null) node.left.next = node.right;
+            if (node.right != null && node.next != null) node.right.next = node.next.left;
+            helper(node.left);
+            helper(node.right);
+        }
+
+        class Node {
+            public int val;
+            public Node left;
+            public Node right;
+            public Node next;
+
+            public Node() {}
+
+            public Node(int _val) {
+                val = _val;
+            }
+
+            public Node(int _val, Node _left, Node _right, Node _next) {
+                val = _val;
+                left = _left;
+                right = _right;
+                next = _next;
+            }
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
